c8afa8c4f495956f11cadae44b34397e4e0aa890
Lucas Pinheiro <54106198+lppinheiro@users.noreply.github.com>
2021-09-02 12:21:07 -0300

    Add files via upload

---
+++ exemploRefit/ICepApiService.cs
@@ -0,0 +1,12 @@
+﻿using Refit;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exemploRefit
+{
+   public interface  ICepApiService
+    {      [Get("/ws/{cep}/json")]
+        Task<CepResponse> GetAddressAsync(string cep);
+    }
+}
